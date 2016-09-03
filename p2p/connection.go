6626b191262e84a4755a517182a37eca88509cff
Paul Snow <snow.paul@gmail.com>
2016-09-03 11:41:03 -0500

    test tweaks

--- p2p/connection.go
+++ p2p/connection.go
@@ -283,7 +283,8 @@ func (c *Connection) dial() bool {
 	address := c.peer.AddressPort()
 	note(c.peer.PeerIdent(), "Connection.dial() dialing: %+v", address)
 	// conn, err := net.Dial("tcp", c.peer.Address)
-	conn, err := net.DialTimeout("tcp", address, time.Second*10)
+	//conn, err := net.DialTimeout("tcp", address, time.Second*10)
+	conn, err := net.Dial("tcp", address)
 	if nil != err {
 		c.setNotes(fmt.Sprintf("Connection.dial(%s) got error: %+v", address, err))
 		return false
