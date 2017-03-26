f25ef25a3a39a0efebb60518bb8baeab4e363426
Brian Deery <brian@factom.org>
2017-03-26 16:17:43 -0500

    tweak entry downloading3

--- state/entrySyncing.go
+++ state/entrySyncing.go
@@ -125,7 +125,7 @@ func (s *State) MakeMissingEntryRequests() {
 			time.Sleep(5 * time.Second)
 		}
 		if s.EntryDBHeightComplete == s.GetHighestSavedBlk() {
-			time.Sleep(5 * time.Second)
+			time.Sleep(10 * time.Second)
 		}
 	}
 }
